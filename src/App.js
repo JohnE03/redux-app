02ec894ebdb3ad73e559238d1c6777be2ffb405d
JohnE03 <2101519@eng.asu.edu.eg>
2025-08-27 15:11:36 +0300

    Updating src/App.js and optimizing useEffect dependencies

--- src/App.js
+++ src/App.js
@@ -1,50 +1,76 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useCallback } from 'react';
 
 function App() {
-  const dispatcher = useDispatch();
+  const dispatcher = useDispatch(); //no need to import store and use store.dispatch
 
-  const state = useSelector((state) => {
-    if(state.counter<=0) return 0;
-    return state.counter;
-  });
+  const globalState = useSelector((state) => state); //now globalState is an object with counter and showCounter properties
+  //no need to use useSelector twice
 
-  const state2 = useSelector((state) => state.showCounter);
+  // const state = useSelector((state) => {
+  //   if(state.counter<=0) return 0;
+  //   return state.counter;
+  // });
 
-  const increaseHandler = () => {
+  // const state2 = useSelector((state) => state.showCounter);
+
+  // const increaseHandler = () => {
+  //   const action = {
+  //     type: 'increase',
+  //     payload: 1
+  //   }
+  //   dispatcher(action);
+  // }
+
+  // const decreaseHandler = () => {
+  //   const action = {
+  //     type: 'decrease',
+  //     payload: -1
+  //   }
+  //   dispatcher(action);
+  // }
+
+
+  const counterHandler = useCallback((type, payload) => {
     const action = {
-      type: 'increase',
-      payload: 1
+      type: type,
+      payload: payload
     }
     dispatcher(action);
-  }
+    // or dispatcher({type, payload}); (ES6 syntax)
+  }, [dispatcher]);
 
-  const decreaseHandler = () => {
-    const action = {
-      type: 'decrease',
-      payload: -1
-    }
-    dispatcher(action);
-  }
+  useEffect(() => {
+    console.log("useEffect fired");
+    counterHandler("increase", 5);
+  }, [counterHandler]);
 
   const toggleCounter = () => {
     const action = {
       type: 'toggle',
-      payload: !state2
+      payload: !globalState.showCounter
     }
     dispatcher(action);
+  }
+
+  const handleNegative = (state) => {
+    if(state<=0) {
+      return "NAN";
+    }
+    return state;
   }
 
   return (
     <div className="App">
       <h1>Hello Redux Basic</h1>
       {
-        state2 &&
+        globalState.showCounter &&
         <>
-          <div className="counter">Counter: {state}</div>
+          <div className="counter">Counter: {handleNegative(globalState.counter)}</div>
           <div>
-            <button id="increase" onClick={increaseHandler}>increase +</button>
-            <button id="decrease" onClick={decreaseHandler}>decrease -</button>
+            <button id="increase" onClick={()=>counterHandler("increase",1)}>increase +</button>
+            <button id="decrease" onClick={()=>counterHandler("decrease",-1)}>decrease -</button>
           </div>
         </>
       }
