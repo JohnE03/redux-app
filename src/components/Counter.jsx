82cd14e6c2881aaae2822d9c20d10b3a2e54f7cb
JohnE03 <2101519@eng.asu.edu.eg>
2025-08-27 16:18:50 +0300

    using @reduxjs/toolkit to improve state management

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useCallback } from 'react';
+//import { useEffect, useCallback } from 'react';
+import {increase, decrease} from '../store/index'
 
 function Counter() {
   const dispatcher = useDispatch(); //no need to import store and use store.dispatch
-
   const globalState = useSelector((state) => state); //now globalState is an object with counter and showCounter properties
+
   //no need to use useSelector twice
 
   // const state = useSelector((state) => {
@@ -32,50 +33,46 @@ function Counter() {
   // }
 
 
-  const counterHandler = useCallback((type, payload) => {
-    const action = {
-      type: type,
-      payload: payload
-    }
-    dispatcher(action);
-    // or dispatcher({type, payload}); (ES6 syntax)
-  }, [dispatcher]);
+  // const counterHandler = useCallback((type, payload) => {
+  //   const action = {
+  //     type: type,
+  //     payload: payload
+  //   }
+  //   dispatcher(action);
+  //   // or dispatcher({type, payload}); (ES6 syntax)
+  // }, [dispatcher]);
 
-  useEffect(() => {
-    console.log("useEffect fired");
-    counterHandler("increase", 5);
-  }, [counterHandler]);
+  // useEffect(() => {
+  //   console.log("useEffect fired");
+  //   counterHandler("increase", 5);
+  // }, [counterHandler]);
 
-  const toggleCounter = () => {
-    const action = {
-      type: 'toggle',
-      payload: !globalState.showCounter
-    }
-    dispatcher(action);
-  }
+  // const toggleCounter = () => {
+  //   const action = {
+  //     type: 'toggle',
+  //     payload: !globalState.showCounter
+  //   }
+  //   dispatcher(action);
+  // }
 
-  const handleNegative = (state) => {
-    if(state < 0) {
-      return "NAN, <0";
-    }
-    return state;
-  }
+  // const handleNegative = (state) => {
+  //   if(state < 0) {
+  //     return "NAN, <0";
+  //   }
+  //   return state;
+  // }
 
   return (
     <div className="App">
       <h1>Hello Redux Basic</h1>
-      {
-        globalState.showCounter &&
-        <>
-          <div className="counter">Counter: {handleNegative(globalState.counter)}</div>
-          <div>
-            <button id="increase" onClick={()=>counterHandler("increase",1)}>increase +</button>
-            <button id="decrease" onClick={()=>counterHandler("decrease",-1)}>decrease -</button>
-          </div>
-        </>
-      }
+      <div className="counter">Counter: {globalState.counter}</div>
+        <div>
+          <button id="increase" onClick ={ () => dispatcher(increase(5)) }>increase +</button>
+          <button id="decrease" onClick ={ () => dispatcher(decrease(-4)) }>decrease -</button>
+        </div>
+      
       <div>
-        <button className='btn' onClick={toggleCounter}>Hide/Show counter number</button>
+        <button className='btn'>Hide/Show counter number</button>
       </div>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,43 @@
-import {createStore} from 'redux';
+import {createSlice, createStore} from '@reduxjs/toolkit';
 
 const initState = {
-  counter: 0,
-  showCounter: true
+  counter: 0
 };
 
-const counterReducer = (state = initState, action) => {
-    if (action.type === 'increase') {
-        return {
-        ...state,
-        counter: state.counter + action.payload
-        };
+//createSlice is a function that accepts a slice name, an initial state and an object full of reducer functions
+const counterSlice = createSlice({ //must use these keys (can't change their names)
+    name: "counter", //indicates which slice of state we are managing
+    initialState: initState,
+    reducers: {
+      increase: (state, action)=>{state.counter += action.payload},
+      decrease: (state, action)=>{state.counter += action.payload}
     }
-    if (action.type === 'decrease') {
-        return {
-        ...state,
-        counter: state.counter + action.payload
-        };
-    }
-    if (action.type === 'toggle') {
-        return {
-        ...state,
-        showCounter: action.payload
-        // or showCounter: !state.showCounter (don't pass a payload to dispatcher)
-        };
-    }
-    //console.log(state);
-  return state;
-}
+});
 
-const store = createStore(counterReducer);
+// const counterReducer = (state = initState, action) => {
+//     if (action.type === 'increase') {
+//         return {
+//         ...state,
+//         counter: state.counter + action.payload
+//         };
+//     }
+//     if (action.type === 'decrease') {
+//         return {
+//         ...state,
+//         counter: state.counter + action.payload
+//         };
+//     }
+//     if (action.type === 'toggle') {
+//         return {
+//         ...state,
+//         showCounter: action.payload
+//         // or showCounter: !state.showCounter (don't pass a payload to dispatcher)
+//         };
+//     }
+//     //console.log(state);
+//   return state;
+// }
 
+const store = createStore(counterSlice.reducer); //pass the reducer
+export const {increase, decrease} = counterSlice.actions; //automatically generated action creators based on the reducers defined inside createSlice
 export default store;
