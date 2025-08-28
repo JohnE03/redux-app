608d5866f5db48e0a8a02a4ee167636f6178a72b
JohnE03 <2101519@eng.asu.edu.eg>
2025-08-28 16:36:04 +0300

    Using configureStore to create the store and combining multiple slices

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 //import { useEffect, useCallback } from 'react';
-import {increase, decrease} from '../store/index'
+import {increase, decrease} from '../store/counterSlice'
 
 function Counter() {
   const dispatcher = useDispatch(); //no need to import store and use store.dispatch
-  const globalState = useSelector((state) => state); //now globalState is an object with counter and showCounter properties
-
+  const globalCounterState = useSelector((state) => state.counter); //now globalState is an object with counter and showCounter properties
   //no need to use useSelector twice
 
   // const state = useSelector((state) => {
@@ -65,7 +64,7 @@ function Counter() {
   return (
     <div className="App">
       <h1>Hello Redux Basic</h1>
-      <div className="counter">Counter: {globalState.counter}</div>
+      <div className="counter">Counter: {globalCounterState.counter}</div>
         <div>
           <button id="increase" onClick ={ () => dispatcher(increase(5)) }>increase +</button>
           <button id="decrease" onClick ={ () => dispatcher(decrease(-4)) }>decrease -</button>
